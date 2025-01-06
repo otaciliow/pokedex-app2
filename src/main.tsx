15669267264b41f610b634ad793f70e9649c2c8e
otaciliow <otacilio.dml@hotmail.com>
2025-01-06 18:09:20 -0300

    feat added project's styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
-// import './index.css'
+import './App.css'
 import { router } from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
