19b6b39f981802b8dc7f785f35b401361e52b959
otaciliow <otacilio.dml@hotmail.com>
2025-01-07 18:17:57 -0300

    style: adjusted style files

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
-import './App.css'
+import './styles/App.css'
 import { router } from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
