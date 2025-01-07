6f3672437ed99686bc7b756d3536e556748cfa47
otaciliow <otacilio.dml@hotmail.com>
2025-01-07 16:40:06 -0300

    feat added page's structure

---
+++ src/pages/Pokemon/index.tsx
@@ -0,0 +1,11 @@
+import { IPokemonsProps } from "../../shared/pokemonsProps.interface";
+import { db } from '../../services/firebaseconnection';
+
+export function Pokemon() {
+
+    return (
+        <>
+        <h1>Hello Kanto!</h1>
+        </>
+    )
+}
